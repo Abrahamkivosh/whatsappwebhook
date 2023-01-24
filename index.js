737dd222b62dacff56eb12b28241dec745be9904
abrahamkivosh <abrahamkivosh@gmail.com>
2023-01-24 22:07:02 +0300

    added get and post for webhook

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const body_parser = require("body-parser")
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+const token = process.env.TOKEN
+const my_token = process.env.MY_TOKEN
+
+
+const  localhost = "http://localhost:" + port
+app.use(body_parser.json())
+
+app.listen(port, () => console.log(`Listening on port ${port} \r \n${localhost}`))
+app.get("/", (req, res) => {
+    res.send(`Access the webhook at ${localhost}/webhook`)
+})
+app.get("/webhook", (req, res) => {
+    let mode = req.query["hub.mode"]
+    let token = req.query["hub.verify_token"]
+    let challenge = req.query["hub.challenge"]
+    if (mode && token) {
+        if (mode === "subscribe" && token === my_token) {
+            console.log("Webhook verified")
+            res.status(200).send(challenge)
+        }else{
+            res.sendStatus(403)
+            console.log("Webhook not verified")
+        }
+
+    }
+})
+
+app.post("/webhook", (req, res) => {
+    let body = req.body
+    if (body.object === "page") {
+        body.entry.forEach(entry => {
+            let phone_no_id = entry.changes[0].value.metadata.phone_number_id
+            let from = entry.changes[0].value.messages[0].from
+            let message_body = entry.changes[0].value.messages[0].text.body
+
+            axios({
+                method: "post",
+                url: "https://graph.facebook.com/v13.6/me/messages"+ phone_no_id + "/messages?access_token"+ token,
+                data: {
+                    messaging_product: "whatsapp",
+                    to: from,
+                    text:{
+                        body: "Hello, I am a bot. You sent me this message: " 
+                    }
+                },
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            res.sendStatus(200)
+            console.log("Message sent")
+        })
+        res.status(200).send("EVENT_RECEIVED")
+    }else{
+        res.sendStatus(404)
+    }
+})
