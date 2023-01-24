b087ceb763cb4d31730dcbe224f85739f7c6501b
abrahamkivosh <abrahamkivosh@gmail.com>
2023-01-24 22:48:27 +0300

    fixed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const body_parser = require("body-parser")
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 const token = process.env.TOKEN
 const my_token = process.env.MY_TOKEN
 
